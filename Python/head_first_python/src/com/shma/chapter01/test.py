d939e7e056e7bde6b0ed5edbc7fc35bb55fb2c02
shma1664 <616059480@qq.com>
2015-10-13 09:48:00 +0800

    head first python

---
+++ Python/head_first_python/src/com/shma/chapter01/test.py
@@ -0,0 +1,26 @@
+'''
+Created on 2015年9月25日
+
+@author: admin
+'''
+from pip._vendor.distlib.compat import raw_input
+if True :
+    print("Answer")
+    print("True")
+else :
+    print("Answer")
+    print("False")
+    
+#多行显示
+
+item01 = '我是中国人'
+item02 = '我深爱着我的祖国'
+
+item = item01 + \
+       item02
+       
+print(item)
+
+#等待用户输入
+raw_input("\n\n请输入您的名字");
+    
