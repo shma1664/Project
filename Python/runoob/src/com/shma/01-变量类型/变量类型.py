9e744bc747d477a4cbea642d925fbc547ea15e97
shma1664 <616059480@qq.com>
2015-10-15 18:23:50 +0800

    变量类型

---
+++ Python/runoob/src/com/shma/01-变量类型/变量类型.py
@@ -0,0 +1,34 @@
+'''
+Created on 2015年10月13日
+@author: shma
+
+'''
+
+''' 变量赋值 '''
+counter = 100; # 整型赋值
+miles = 100.2; #浮点数赋值
+name = "张三";
+school = '清华大学'; #字符串复制
+
+print(counter);
+print(miles);
+print("name:" + name + ", school:" + school);
+
+
+''' 多变量赋值 '''
+a = b = c = 1;
+print(a);
+print(b);
+print(c);
+a = 2;
+print(a);
+print(b);
+print(c);
+
+d, e, f = 3, 2.1, 'shma';
+print(d);
+print(e);
+print(f);
+
+
+
---
+++ Python/runoob/src/com/shma/01-变量类型/字符串.py
@@ -0,0 +1,13 @@
+'''
+Created on 2015年10月13日
+@author: admin
+'''
+
+newStr = 'Hello World';
+
+print(newStr); #输出完整的字符串
+print(newStr[0]); #输出第一个字符
+print(newStr[1:5]); #输出[1,5)字符
+print(newStr[3:]); #输出[3,~字符
+print(newStr * 2); #输出2遍
+print(newStr + ", python") #输出连续字符串
